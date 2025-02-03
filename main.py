87eb021ea2590565aa5948bd2548c393152ba947
Gromer <rus.solovey.09@mail.ru>
2025-02-03 09:37:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 # подключаем библиотеку для работы с запросами
 import requests
-# указываем город
-city = str(input("Введите название город(например: Москва): "))
-# формируем запрос
-url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
-# отправляем запрос на сервер и сразу получаем результат
-weather_data = requests.get(url).json()
-# получаем данные о температуре и о том, как она ощущается
-temperature = round(weather_data['main']['temp'])
-temperature_feels = round(weather_data['main']['feels_like'])
-# выводим значения на экран
-print('Сейчас в городе', city, str(temperature), '°C')
-print('Ощущается как', str(temperature_feels), '°C')
-input("Нажмите Enter для завершения...")
+from tkinter import *
+from tkinter import ttk
+ 
+root = Tk()
+root.title("METANIT.COM")
+root.geometry("400x400")
+
+def getTemperature(city):
+	url = 'https://api.openweathermap.org/data/2.5/weather?q='+ city_var.get() + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+	weather_data = requests.get(url).json()
+	temperature = round(weather_data['main']['temp'])
+	return temperature
+
+def selected(event):
+    # получаем выделенный элемент
+    selection = combobox.get()
+    city_var.set(selection)
+    label["text"] = f"Температура: {getTemperature(city_var.get())}"
+    print(selection)
+
+city = ["Москва", "Курск", "Курчатов", "Владивосток"]
+label = ttk.Label()
+label.pack(anchor=NW, fill=X, padx=5, pady=5)
+
+city_var = StringVar(value = city[0])
+combobox = ttk.Combobox(textvariable = city_var, values = city, state = "readonly")
+combobox.pack(anchor=CENTER, padx=6, pady=6)
+
+#temperature_feels = round(weather_data['main']['feels_like'])
+combobox.bind("<<ComboboxSelected>>", selected)
+ 
+root.mainloop()
